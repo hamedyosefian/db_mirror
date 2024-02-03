ef25ce7394c099bab47bc54373aacce06c4eb7f6
hamedyousefian <hamed77yosefian@gmail.com>
2024-02-03 14:10:17 +0330

    remove check_same_thread

--- app/database.py
+++ app/database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    settings.SQLALCHEMY_DATABASE_URL,
+    # connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
